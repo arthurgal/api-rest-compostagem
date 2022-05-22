89bef1e67a8da1a3e423ee72d75b2c21989496e7
Arthur Galdino <arthurgaldinoo@hotmail.com>
2022-05-21 22:40:08 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/apirest/compostagem/CompostagemApplication.java
@@ -0,0 +1,12 @@
+package br.com.apirest.compostagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompostagemApplication {
+
+    public static void main(String[] args){SpringApplication.run(CompostagemApplication.class, args);
+    }
+
+}
