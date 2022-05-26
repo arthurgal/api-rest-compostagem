19d8e0d1435f383f5fb44dfb1c63c2de59eb15b4
arthurgal <arthurgaldinoo@hotmail.com>
2022-05-26 12:05:37 -0300

    bean nullable

--- src/main/java/br/com/apirest/compostagem/model/Usuario.java
+++ src/main/java/br/com/apirest/compostagem/model/Usuario.java
@@ -3,6 +3,7 @@ package br.com.apirest.compostagem.model;
 import br.com.apirest.compostagem.validacaoCpf.Cpf;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.constraints.Email;
@@ -11,7 +12,9 @@ import javax.validation.constraints.Email;
 @Setter
 public class Usuario {
 
+    @Nullable
     private String nome;
+    @Nullable
     private String endereco;
     @Cpf
     private String cpf;
