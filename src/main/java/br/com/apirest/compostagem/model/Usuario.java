dc289a31f7094770dadb0c79da41e774a0a76abe
arthurgal <arthurgaldinoo@hotmail.com>
2022-05-26 11:05:41 -0300

    Bean Email

--- src/main/java/br/com/apirest/compostagem/model/Usuario.java
+++ src/main/java/br/com/apirest/compostagem/model/Usuario.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.constraints.Email;
+
 @Getter
 @Setter
 public class Usuario {
@@ -13,6 +15,7 @@ public class Usuario {
     private String endereco;
     @Cpf
     private String cpf;
+    @Email
     private String email;
 
 }
